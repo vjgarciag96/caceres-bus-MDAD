a2f89799bceb7b6c03b1e0c85d3adb4d57261ab8
vjgarciag96 <vjgarciag96@gmail.com>
2018-04-07 01:55:51 +0200

    Deleted test method

--- api.py
+++ api.py
@@ -17,36 +17,4 @@ def map():
     return render_template('map.html', mymap=mymap)
 
 
-@app.route('/flaskmap')
-def mapview():
-    # creating a map in the view
-    mymap = Map(
-        identifier="view-side",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[(37.4419, -122.1419)],
-        fullscreen_control=True
-    )
-    sndmap = Map(
-        identifier="sndmap",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[
-            {
-                'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-                'lat': 37.4419,
-                'lng': -122.1419,
-                'infobox': "<b>Hello World</b>"
-            },
-            {
-                'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-                'lat': 37.4300,
-                'lng': -122.1400,
-                'infobox': "<b>Hello World from other place</b>"
-            }
-        ]
-    )
-    return render_template('flaskmap.html', mymap=mymap, sndmap=sndmap)
-
-
 app.run(host='0.0.0.0', port=8081, threaded=True)
