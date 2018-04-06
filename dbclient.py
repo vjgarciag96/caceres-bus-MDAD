65ab9cb75acaaddfc50b5e46932c2390413b49e0
albeerto26 <albertocarreras6@gmail.com>
2018-04-07 00:00:58 +0200

    Updated query methods

--- dbclient.py
+++ dbclient.py
@@ -17,6 +17,27 @@ class Client:
             enable_logging=True
         )
 
-    def get_flights(self):
-        result = self.client.db('_system').aql.execute('FOR s IN flights RETURN s')
-        print(result)
+    def get_best_flight(self, origin, target, day, month):
+        result = self.client.db('_system').aql.execute(
+'''FOR v, e, p IN 2 OUTBOUND 'airports/'''+origin+'''' flights FILTER v._id == 'airports/'''+target+''''
+FILTER p.edges[*].Month ALL == '''+month+'''
+FILTER p.edges[*].DayofMonth ALL == '''+day+'''
+LET flightTime = DATE_DIFF(p.edges[0].DepTimeUTC, p.edges[1].ArrTimeUTC, 'i') SORT flightTime ASC
+LIMIT 5
+RETURN { flight: p, time: flightTime }'''
+)
+        collection = list()
+        collection.append([student for student in result])
+        return collection
+
+    def get_all_airports(self):
+        result = self.client.db('_system').aql.execute(
+'''FOR airport IN airports
+RETURN {
+    name:airport.airport,
+    id:airport._key
+}'''
+        )
+        collection = list()
+        collection.append([student for student in result])
+        return collection
