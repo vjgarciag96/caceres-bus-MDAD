3b1b57dce670e98aa624ce8c59dedfb27b51330f
vjgarciag96 <vjgarciag96@gmail.com>
2018-04-07 01:51:59 +0200

    Implemented map view with menu for searchs

--- api.py
+++ api.py
@@ -1,12 +1,52 @@
 from flask import Flask
 from flask import render_template
+from flask_googlemaps import GoogleMaps, Map
 
 app = Flask(__name__)
+GoogleMaps(app, key='AIzaSyBo6E0nwsf0ijiCQZJBsq8Rs7Ptt4Na6l4')
 
 
 @app.route('/')
 def map():
-    return render_template('map.html')
+    mymap = Map(
+        identifier="view-side",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[(37.4419, -122.1419)]
+    )
+    return render_template('map.html', mymap=mymap)
+
+
+@app.route('/flaskmap')
+def mapview():
+    # creating a map in the view
+    mymap = Map(
+        identifier="view-side",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[(37.4419, -122.1419)],
+        fullscreen_control=True
+    )
+    sndmap = Map(
+        identifier="sndmap",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[
+            {
+                'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+                'lat': 37.4419,
+                'lng': -122.1419,
+                'infobox': "<b>Hello World</b>"
+            },
+            {
+                'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+                'lat': 37.4300,
+                'lng': -122.1400,
+                'infobox': "<b>Hello World from other place</b>"
+            }
+        ]
+    )
+    return render_template('flaskmap.html', mymap=mymap, sndmap=sndmap)
 
 
 app.run(host='0.0.0.0', port=8081, threaded=True)
