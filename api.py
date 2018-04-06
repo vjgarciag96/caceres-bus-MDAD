04b6286c192d2faf46b68ec62af70a9ad4815e49
vjgarciag96 <vjgarciag96@gmail.com>
2018-04-06 20:49:25 +0200

    Created initial map

--- api.py
+++ api.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-app.run(host='localhost',port='8081')
+@app.route('/')
+def map():
+    return render_template('map.html')
+
+
+app.run(host='0.0.0.0', port=8081, threaded=True)
