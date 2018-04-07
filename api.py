ad5a789cde9ce70eeb9e5005f9a5ae7b5658333f
vjgarciag96 <vjgarciag96@gmail.com>
2018-04-07 02:18:03 +0200

    Implemented sample form

--- api.py
+++ api.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 from flask_googlemaps import GoogleMaps, Map
 
 app = Flask(__name__)
@@ -14,7 +13,19 @@ def map():
         lng=-122.1419,
         markers=[(37.4419, -122.1419)]
     )
-    return render_template('map.html', mymap=mymap)
+
+    airports = [{'name': 'Malta', 'id': 'M75'}, {'name': 'Bismarck Municipal', 'id': 'BIS'}];
+    return render_template('map.html', mymap=mymap, airports=airports)
+
+
+@app.route('/', methods=['POST'])
+def map_search_post():
+    date = request.form['date']
+    origin = request.form['origin']
+    target = request.form['target']
+    print("origen = " + str(origin))
+    print("destino = " + str(target))
+    print("date = " + str(date))
 
 
 app.run(host='0.0.0.0', port=8081, threaded=True)
