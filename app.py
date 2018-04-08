3fd1771eda0d718d47e673a6cf4283254031fbd9
vjgarciag96 <vjgarciag96@gmail.com>
2018-04-09 01:00:14 +0200

    Fixed error on first map load

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 from flask import Flask, render_template, request
-from flask_googlemaps import GoogleMaps, Map
 from dbclient import arangodb_client
 from datetime import datetime
 import json
 
 app = Flask(__name__)
-GoogleMaps(app, key='AIzaSyBo6E0nwsf0ijiCQZJBsq8Rs7Ptt4Na6l4')
-
 
 @app.route('/')
 def map():
-    return render_template('map.html', airports=get_all_airports())
-
+    return render_template('map.html', data=json.dumps([]), airports=get_all_airports())
 
 @app.route('/', methods=['POST'])
 def map_search_post():
@@ -32,7 +28,6 @@ def map_search_post():
         coordinates = list()
         for flight in flightroute['flight']['vertices']:
             coordinates.append({'lat': flight['lat'], 'long': flight['long']})
-            # coordinates -> lista de lat, long para la línea, time -> tiempo del vuelo mételo donde sea
         for coordinate in coordinates:
             print(coordinate)
 
@@ -47,23 +42,5 @@ def map_search_post():
 
 def get_all_airports():
     return sorted(arangodb_client().get_all_airports(), key=lambda student: student['name'])
-
-
-def create_map_from_flights_coordinates(coordinates):
-    markers = list()
-    for coordinate in coordinates:
-        marker = {'icon': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
-                  'lat': str(coordinate['lat']),
-                  'lng': str(coordinate['long'])}
-        markers.append(marker)
-
-    mymap = Map(
-        identifier="sndmap",
-        lat=46.77411111,
-        lng=-100.7467222,
-        markers=markers)
-
-    return mymap
-
 
 app.run(host='0.0.0.0', port=8081, threaded=True)
