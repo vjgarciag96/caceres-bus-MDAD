0bf47466c80875ece3e5968616121b66c8f5b39a
vjgarciag96 <vjgarciag96@gmail.com>
2018-04-17 11:14:05 +0200

    Added markers for route intermediate points and simplified business logic dropping google directions

--- app.py
+++ app.py
@@ -25,16 +25,16 @@ def map_search_post():
     bus_path = client.get_shortest_bus_path(origin, target, time)
 
     if bus_path:
-        print(bus_path)
-
         # time = bus_route['time']
         # trip_time = 'Tiempo estimado de viaje: ' + str(time) + ' min'
         coordinates = list()
+        for coordinate_point in bus_path:
+            coordinates.append({'lat': coordinate_point['lat'], 'lon': coordinate_point['lon']})
 
-        for index in range(len(bus_path) - 1):
-            if index + 1 <= len(bus_path) - 1:
-                coordinates = coordinates + get_real_bus_path_coordinates_between_locations(bus_path[index],
-                                                                                            bus_path[index + 1])
+        # for index in range(len(bus_path) - 1):
+        #     if index + 1 <= len(bus_path) - 1:
+        #         coordinates = coordinates + get_real_bus_path_coordinates_between_locations(bus_path[index],
+        #                                                                                     bus_path[index + 1])
 
         return render_template('map.html', data=json.dumps(coordinates), trip_time=0, stops=get_all_stops())
 
@@ -48,23 +48,23 @@ def get_all_stops():
     return arangodb_client().get_all_stops()
 
 
-def get_real_bus_path_coordinates_between_locations(start_location, end_location):
-    path_coordinates = list()
-
-    latlng_str = '{},{}'
-    direction = requests.get(GOOGLE_DIRECTION_ENDPOINT_WITH_PARAMS.
-                             format(latlng_str.format(start_location['lat'], start_location['lon']),
-                                    latlng_str.format(end_location['lat'], end_location['lon']),
-                                    GOOGLE_DIRECTION_API_KEY))
-    directions_dict = direction.json()
-
-    path_coordinates.append({'lat': start_location['lat'], 'lon': start_location['lon']})
-
-    for step in directions_dict['routes'][0]['legs'][0]['steps']:
-        latlng = step['end_location']
-        path_coordinates.append({'lat': latlng['lat'], 'lon': latlng['lng']})
-
-    return path_coordinates
+# def get_real_bus_path_coordinates_between_locations(start_location, end_location):
+#     path_coordinates = list()
+#
+#     latlng_str = '{},{}'
+#     direction = requests.get(GOOGLE_DIRECTION_ENDPOINT_WITH_PARAMS.
+#                              format(latlng_str.format(start_location['lat'], start_location['lon']),
+#                                     latlng_str.format(end_location['lat'], end_location['lon']),
+#                                     GOOGLE_DIRECTION_API_KEY))
+#     directions_dict = direction.json()
+#
+#     path_coordinates.append({'lat': start_location['lat'], 'lon': start_location['lon']})
+#
+#     for step in directions_dict['routes'][0]['legs'][0]['steps']:
+#         latlng = step['end_location']
+#         path_coordinates.append({'lat': latlng['lat'], 'lon': latlng['lng']})
+#
+#     return path_coordinates
 
 
 app.run(host='0.0.0.0', port=8081, threaded=True)
